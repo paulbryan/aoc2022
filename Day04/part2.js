5d726d8639111c573c6dd1d7f124dfe0917e4a47
Paul Bryan <paul@fghdev.com>
2022-12-06 10:02:54 +0800

    Day 4

---
+++ Day04/part2.js
@@ -0,0 +1,74 @@
+//https://adventofcode.com/
+//day four
+let commonItems=0
+
+
+const inputLines = require('fs').readFileSync('input.txt', 'UTF-8').split(/\r?\n/)
+// const inputLines=[
+//   '2-4,6-8',
+// '2-3,4-5',
+// '5-7,7-9',
+// '2-8,3-7',
+// '6-6,4-6',
+// '2-6,4-8'
+// ]
+const getArray=(start,end)=>{
+  return [...Array(end-start+1).keys()].map(x=>x+start)
+}
+inputLines.forEach(currentInputLine => {
+  if (currentInputLine===""){
+    console.log(commonItems)
+  }else{
+   const elf1=currentInputLine.split(',')[0]
+   const elf2=currentInputLine.split(',')[1]
+   const elf1Min=parseInt(elf1.split('-')[0])
+   const elf1Max=parseInt(elf1.split('-')[1])
+   const elf2Min=parseInt(elf2.split('-')[0])
+   const elf2Max=parseInt(elf2.split('-')[1])
+   const efl1Array=getArray(elf1Min,elf1Max)
+   const efl2Array=getArray(elf2Min,elf2Max)
+    // console.log(elf1Min,elf1Max,efl1Array)
+    // console.log(elf2Min,elf2Max,efl2Array)
+    console.log(efl1Array,efl2Array,efl1Array.filter((item)=>{return efl2Array.indexOf(item)>=0}))
+    if (efl1Array.filter((item)=>{return efl2Array.indexOf(item)>=0}).length>0){
+      commonItems+=1
+    }
+  //  if (elf1Max>=elf2Min){
+  //   commonItems+=1
+  //   console.log(elf1,elf2,elf1Min,elf1Max,elf2Min,elf2Max)
+  //  }
+  }
+})
+console.log(commonItems)
+// const inputLines=[
+//   'vJrwpWtwJgWrhcsFMMfFFhFp',
+// 'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+// 'PmmdzqPrVvPwwTWBwg',
+// 'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+// 'ttgJtRGJQctTZtZT',
+// 'CrZsJsPPZsGzwwsLwLmpwMDw',''
+// ]
+// inputLines.forEach(currentInputLine => {
+//   if (currentInputLine===""){
+//     console.log(commonItems)
+//   }else{
+//     const lineLength=currentInputLine.length
+//     const packLength=lineLength/2
+//     const pack1=currentInputLine.substring(0,packLength)
+//     const pack2=currentInputLine.substring(packLength,lineLength)
+//     console.log(pack1,pack2,pack1.length,pack2.length,currentInputLine.length)
+//     currentCommonItems=new Set()
+//     for (let i=0;i<pack1.length;i++){
+        
+//       const pack1Char=pack1.substring(i,i+1)
+//       for (let j=0;j<pack2.length;j++){
+        
+//         const pack2Char=pack2.substring(j,j+1)
+//         if (pack1Char===pack2Char){
+//           currentCommonItems.add(pack1Char)      
+//         }
+//       }
+//     }
+//     commonItems.push(...currentCommonItems)
+//   }
+// })
