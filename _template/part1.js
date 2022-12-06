78298fd7a2b570b8e22bc0649490e321512cf763
Paul Bryan <paul@fghdev.com>
2022-12-06 16:33:13 +0800

    template

---
+++ _template/part1.js
@@ -0,0 +1,14 @@
+const inputLines = require('fs').readFileSync('input.txt', 'UTF-8').split(/\r?\n/)
+const inputLines2=['','']
+
+inputLines.forEach(currentInputLine => {
+    if (currentInputLine===''){
+        
+    }
+    else {
+        for(let i=0;i<currentInputLine.length;i++){
+           
+        }
+    }
+
+})
